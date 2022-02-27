64c00818778f27b66232921c0a2828f53542ef70
lt <1029563229@qq.com>
2022-02-27 16:24:37 +0800

    加入管道验证器

--- src/shop/dto/create-shop.dto.ts
+++ src/shop/dto/create-shop.dto.ts
@@ -1,14 +1,18 @@
+import { IsNotEmpty } from 'class-validator';
 import { ShopType } from '../models/shop.entity';
 
 export class QueryShopDto {
+  @IsNotEmpty({ message: 'id 不能为空' })
   id: number;
 }
 
 export class CreateShopDto {
+  @IsNotEmpty({ message: '店铺名称不能为空' })
   name: string;
 
   description: string;
 
+  @IsNotEmpty({ message: '店铺类型不能为空' })
   type: ShopType;
 
   poster: string;
@@ -17,14 +21,18 @@ export class CreateShopDto {
 
   tags: string[];
 
+  @IsNotEmpty({ message: '店铺评分不能为空' })
   score: number;
 
   evaluation: string;
 
+  @IsNotEmpty({ message: '店铺地址不能为空' })
   address: string;
 
+  @IsNotEmpty({ message: '店铺经度不能为空' })
   longitude: number;
 
+  @IsNotEmpty({ message: '店铺纬度不能为空' })
   latitude: number;
 }
 
