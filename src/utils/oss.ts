2b9b605fb3e294a3f47d136e50e8137ff068cabb
lt <1029563229@qq.com>
2022-03-20 17:52:07 +0800

    加上等待时间

--- src/utils/oss.ts
+++ src/utils/oss.ts
@@ -21,12 +21,12 @@ const hash = (str: string): string => {
     .digest('hex');
 };
 
-export const upload = (file: Express.Multer.File): string => {
+export const upload = async (file: Express.Multer.File): Promise<string> => {
   const stream = new Duplex();
   stream.push(file.buffer);
   stream.push(null);
   const fileName = hash(file.originalname) + path.extname(file.originalname);
-  client.putStream(fileName, stream);
+  await client.putStream(fileName, stream);
   const url = `http://${ossConfig.bucket}.${ossConfig.region}.aliyuncs.com/${fileName}`;
   return url;
 };
